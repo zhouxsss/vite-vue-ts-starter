39f2ba4763c40051e1b98638af724cdb001efc7d
zhouxs <zhouxs@yunphant.com>
2021-07-19 10:09:14 +0800

    feat: antd locale provider

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <router-view />
+  <ConfigProvider :locale="getLocale">
+    <router-view />
+  </ConfigProvider>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
+  import { defineComponent, onMounted, computed } from 'vue'
+  import { useStore } from 'vuex'
+  import { ConfigProvider } from 'ant-design-vue'
 
-export default defineComponent({
-  name: 'App',
-  setup() {
-    onMounted(() => {
-      // todo： 需要添加移动端限制
-      const htmlDom = document.querySelector('html') as HTMLElement
-      htmlDom.style.fontSize = '4.26vw'
-    })
-    return {}
-  }
-})
+  export default defineComponent({
+    name: 'App',
+    components: { ConfigProvider },
+    setup() {
+      const store = useStore()
+      onMounted(() => {
+        // todo： 需要添加移动端限制
+        const htmlDom = document.querySelector('html') as HTMLElement
+        htmlDom.style.fontSize = '4.26vw'
+      })
+      return {
+        locale: computed(() => store.getters.getLocale)
+      }
+    }
+  })
 </script>
 
 <style>
