9ab11a3fcd08d82bc07157bc38e260c4acf21328
zhouxs <zhouxs@yunphant.com>
2021-07-19 14:44:21 +0800

    feat: appProvider

---
+++ src/components/AppProvider.vue
@@ -0,0 +1,14 @@
+<script>
+import { ref } from 'vue'
+import { defineComponent } from '@vue/composition-api'
+import { createContext } from '/@/hooks/useContext'
+
+export default defineComponent({
+  setup(props, { slot }) {
+    const isMobile = ref(false)
+    createContext({ isMobile })
+
+    return () => slot.default?.()
+  },
+})
+</script>
